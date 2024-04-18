f4ec60c6cb5c6ab06fc5b716e3af1745dc74f8aa
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-04-17 23:23:46 -0400

    Commit Cuarto

--- onlyflans_project/onlyflans_web/models.py
+++ onlyflans_project/onlyflans_web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class Flan(models.Model):
+
+    flan_uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField(unique=True)
+    is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- onlyflans_project/onlyflans_web/views.py
+++ onlyflans_project/onlyflans_web/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from .models import Flan
 
 # Create your views here.
 def indice(request):
-    return render(request, "index.html", {})
+
+    # Obtiene flanes públicos
+    postres_publicos = Flan.objects.filter(is_private = False)
+
+    return render(request, "index.html", {
+        'postres_publicos': postres_publicos
+    })
+
+def bienvenido(request):
+
+    # Obtiene flanes privados
+    postres_privados = Flan.objects.filter(is_private = True)
+
+    return render(request, "welcome.html", {
+        'postres_privados': postres_privados
+    })
 
 def acerca(request):
     return render(request, "about.html", {})
-
-def bienvenido(request):
-    return render(request, "welcome.html", {})
